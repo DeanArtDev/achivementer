9f30b9a23c1f615b0ee80e33e2b4ade90b128381
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-01-16 02:08:59 +0700

    add BaseInput Validator

---
+++ src/UI/BaseInput/hooks/useInputValidate.ts
@@ -0,0 +1,79 @@
+import { useEffect, useRef, useState } from "react";
+import { InputValidationOptions } from "type";
+import { useEffectOnce } from "react-use";
+
+type UseInputValidate = [boolean, boolean, (value: string) => boolean, () => boolean];
+
+const checkByRegExp = (value: string, regexp: string): boolean => {
+  const regExp = new RegExp(regexp);
+  return regExp.test(value);
+};
+
+export default function useInputValidate(
+  value: string,
+  inputValidateOptions?: InputValidationOptions
+): UseInputValidate {
+  const { regexp, initialValue, require } = inputValidateOptions ?? {};
+  const [isValid, setIsValid] = useState(initialValue ?? false);
+  const [isShowError, setIsShowError] = useState(initialValue ? false : !isValid);
+  const isFirstChange = useRef(false);
+
+  const validate = (newValue: string): boolean => {
+    const value = newValue.trim();
+    if (value === "") {
+      setIsValid(false);
+      return false;
+    }
+
+    if (regexp) {
+      const checkedValue = checkByRegExp(value, regexp);
+      setIsValid(checkedValue);
+      return checkedValue;
+    }
+
+    setIsValid(true);
+    return true;
+  };
+
+  const isCanChangeField = (newValue: string): boolean => {
+    isFirstChange.current = true;
+    const valid = validate(newValue);
+
+    if (!valid && newValue.trim() === "") {
+      return true;
+    }
+
+    return valid;
+  };
+
+  const validatingCallback = () => {
+    setIsShowError(!isValid);
+    return isValid;
+  };
+
+  useEffect(() => {
+    const trimmedValue = value.trim();
+    if ((trimmedValue === "" && isValid) || !isFirstChange.current) {
+      setIsShowError(false);
+      return;
+    }
+
+    if (trimmedValue === "" && require) {
+      setIsShowError(true);
+      return;
+    }
+
+    if (!isValid && trimmedValue.length > 1 && regexp && checkByRegExp(trimmedValue, regexp)) {
+      setIsShowError(false);
+      return;
+    }
+
+    setIsShowError(!isValid);
+  }, [isValid]);
+
+  useEffectOnce(() => {
+    if (value.trim() !== "") validate(value.trim());
+  });
+
+  return [isValid, isShowError, isCanChangeField, validatingCallback];
+}
