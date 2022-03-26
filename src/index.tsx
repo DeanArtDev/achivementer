846a10d25a8a5ae617953d3a11d2e8a20ec70abb
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-03-26 21:17:59 +0700

    hide dev tools & source maps for production

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.scss';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import disableDevToolsForProductionBuild from "utils/disableDevToolsForProductionBuild";
+import App from "./App";
+
+import "./style/index.scss";
+
+disableDevToolsForProductionBuild();
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
