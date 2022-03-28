69458c2203d31f4f8a1c0eb0b5a7701c88ec4b29
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-03-28 21:23:47 +0700

    change position of Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
-import AuthUserContext from "context/AuthUserContext/AuthUserContext";
 import React from "react";
+import { BrowserRouter as Router } from "react-router-dom";
+import AuthUserContext from "context/AuthUserContext/AuthUserContext";
 import MainRouter from "./router";
 
 export default function App() {
   return (
-    <AuthUserContext>
-      <MainRouter />
-    </AuthUserContext>
+    <Router>
+      <AuthUserContext>
+        <MainRouter />
+      </AuthUserContext>
+    </Router>
   );
 }
--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,7 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router } from "react-router-dom";
-import { Route, Switch, useLocation } from "react-router-dom";
-import { LocationState } from "types";
+import { Route, Switch } from "react-router-dom";
 import PageLogin from "pages/PageLogin";
 import { route } from "./consts";
 import PrivateRoutes from "./PrivateRoutes";
@@ -9,26 +7,22 @@ import FinancialRouter from "./FinancialRouter";
 import PathGuard from "./PathGuard";
 
 export default function MainRouter() {
-  const location = useLocation<LocationState>();
-
   return (
-    <Router>
-      <PathGuard>
-        <Switch location={location}>
-          <Route exact path={route.LOGIN}>
-            <PageLogin />
-          </Route>
+    <PathGuard>
+      <Switch>
+        <Route exact path={route.LOGIN}>
+          <PageLogin />
+        </Route>
 
-          <PrivateRoutes>
-            <Route path={"/finances"}>
-              <FinancialRouter />
-            </Route>
-            <Route path={"*"}>
-              <div>ERROR</div>
-            </Route>
-          </PrivateRoutes>
-        </Switch>
-      </PathGuard>
-    </Router>
+        <PrivateRoutes>
+          <Route path={"/finances"}>
+            <FinancialRouter />
+          </Route>
+          <Route path={"*"}>
+            <div>ERROR</div>
+          </Route>
+        </PrivateRoutes>
+      </Switch>
+    </PathGuard>
   );
 }
