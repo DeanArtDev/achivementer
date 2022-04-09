6b9c0a87a298cd73f34d11f02ccdc93fe3064763
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-04-09 15:27:26 +0700

    add style of Notification Manager

--- src/components/NotificationManager/index.tsx
+++ src/components/NotificationManager/index.tsx
@@ -2,8 +2,11 @@ import React, { useEffect, useState } from "react";
 import emitter from "utils/emitter";
 import usePortal from "hooks/usePortal";
 import { GlobalEmitPayload, ShowNotificationPayload } from "types/emittery";
+import { ReactComponent as NotificationIcon } from "assets/images/icons/new-alert-notification.svg";
 import { GlobalEmit } from "consts";
-// import "./style.scss";
+import "./style.scss";
+
+const HIDDEN_MESSAGE_TIMER = 5000;
 
 export default function NotificationManager() {
   const goToPortal = usePortal();
@@ -17,6 +20,17 @@ export default function NotificationManager() {
     return emitter.on(GlobalEmit.SHOW_NOTIFICATION, handleEmitterShow);
   });
 
+  useEffect(() => {
+    errorMessage && setTimeout(() => setErrorMessage(""), HIDDEN_MESSAGE_TIMER)
+  }, [errorMessage])
+
   if (!errorMessage) return null;
-  return goToPortal(<div className={"notification-manager"}>{errorMessage}</div>);
+  return goToPortal(
+    <div className={"notification-manager container-narrow __x-padding mx-auto"}>
+      <div className={"notification-manager__content py-4"}>
+        <NotificationIcon className={"mr-2"} width={35} height={35} />
+        <span className={"notification-manager__msg"}>{errorMessage}</span>
+      </div>
+    </div>
+  );
 }
