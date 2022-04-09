8e9355d8ca22cad698ba7161635eb729ab48aff6
Dmitriy Korobchenko <deankorobchenko@scrile.com>
2022-04-09 20:17:35 +0700

    change naming

--- src/pages/PageFinances/components/FinancialReportEditor/viewController.ts
+++ src/pages/PageFinances/components/FinancialReportEditor/viewController.ts
@@ -1,32 +1,32 @@
 import { useRef, useState } from "react";
 
 export default function useViewController(): {
-  setValidationParts: (isValid: boolean) => void;
-  setValidationPeriod: (isValid: boolean) => void;
-  isFieldsValid: boolean;
+  setPartsValidation: (isValid: boolean) => void;
+  setPeriodValidation: (isValid: boolean) => void;
+  isAllFieldsValid: boolean;
 } {
-  const [isFieldsValid, setIsFieldsValid] = useState(false);
+  const [isAllFieldsValid, setIsAllFieldsValid] = useState(false);
 
   const validationParts = useRef<boolean>(false);
   const validationPeriod = useRef<boolean>(false);
   const checkAllValidation = () => {
-    if (isFieldsValid && (!validationParts.current || !validationPeriod.current)) {
-      setIsFieldsValid(false);
+    if (isAllFieldsValid && (!validationParts.current || !validationPeriod.current)) {
+      setIsAllFieldsValid(false);
     }
     if (validationParts.current && validationPeriod.current) {
-      setIsFieldsValid(true);
+      setIsAllFieldsValid(true);
     }
   };
 
-  const setValidationParts = (isValid: boolean): void => {
+  const setPartsValidation = (isValid: boolean): void => {
     validationParts.current = isValid;
     checkAllValidation();
   };
 
-  const setValidationPeriod = (isValid: boolean): void => {
+  const setPeriodValidation = (isValid: boolean): void => {
     validationPeriod.current = isValid;
     checkAllValidation();
   };
 
-  return { setValidationParts, setValidationPeriod, isFieldsValid };
+  return { setPartsValidation, setPeriodValidation, isAllFieldsValid };
 }
